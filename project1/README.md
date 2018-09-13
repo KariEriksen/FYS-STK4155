87aeae81e8f442afa7d5e4d88c7c138b22f5013c
Morten Ledum <mortele@student.matnat.uio.no>
2018-09-13 15:26:49 +0200

    Updated requirements to be more in line with Travis defaults so building the virtual image is faster, wrote and tested franke function function

--- project1/README.md
+++ project1/README.md
@@ -1,2 +1,11 @@
+[![Build Status](https://travis-ci.com/mortele/FYS-STK4155.svg?branch=master)](https://travis-ci.com/mortele/FYS-STK4155)
+[![codecov](https://codecov.io/gh/mortele/FYS-STK4155/branch/master/graph/badge.svg)](https://codecov.io/gh/mortele/FYS-STK4155)
+[![Project Status: WIP – Initial development is in progress, but there has not yet been a stable, usable release suitable for the public.](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+
+
+
+
 ### Project 1: Machine learning (*deadline 01.10.2018.*)
 [Exercise test](https://compphysics.github.io/MachineLearning/doc/Projects/2018/Project1/html/Project1-bs.html)
+
+
--- project1/test/test_franke.py
+++ project1/test/test_franke.py
@@ -1,14 +1,42 @@
 import os
 import sys
+import pytest
 import numpy as np
 
 # Add the src/ directory to the python path so we can import the code 
 # we need to test directly as 'from <file name> import <function/class>'
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 
-def test_franke() :
-	from franke import franke
-	x = 1
-	y = 2
-	assert franke(x,y) == 1
+from franke import franke
 
+
+def test_franke() :
+    """Tests the Franke function as implemented in franke.py
+
+    The tests comprise evaluation and comparison against a MATLAB implementation
+    of the same function [1].
+
+    [1] https://www.sfu.ca/~ssurjano/Code/franke2dm.html
+    """
+
+    x = [     0.5185949425105382,
+              0.6489914927123561,
+              0.6596052529083072,
+              0.8003305753524015,
+              0.9729745547638625
+        ]
+    y = [     0.0834698148589140,
+              0.1331710076071617,
+              0.4323915037834617,
+              0.4537977087269195,
+              0.8253137954020456
+        ]
+    f = [     0.4488306370927234,
+              0.4063778975108695,
+              0.4875600327917881,
+              0.4834151909828157,
+              0.0479911637101943
+        ]
+    for i in range(5) :
+        assert franke(x[i], y[i]) == pytest.approx(f[i], abs=1e-15)
+    

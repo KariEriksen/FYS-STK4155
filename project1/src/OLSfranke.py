1fd2688a39f04bc50ccd117abe627399d5a1cf4d
haakoek <haakoek@student.matnat.uio.no>
2018-09-13 16:17:24 +0200

    Added computation of MSE

--- project1/src/OLSfranke.py
+++ project1/src/OLSfranke.py
@@ -1,7 +1,7 @@
 import numpy as np
 from franke import franke
 
-N = 100
+N = 200
 
 x = np.random.rand(N)
 y = np.random.rand(N)
@@ -13,10 +13,12 @@ X = np.c_[np.ones(N),
 		  x**4,x**3*y,x**2*y**2,x*y**3,y**4,
 		  x**5,x**4*y,x**3*y**2,x**2*y**3,x*y**4,y**5]
 
-
 z = franke(x,y)
-
 XT_X = np.linalg.inv(np.dot(X.T,X))
 XT_z = np.dot(X.T,z)
 beta = np.dot(XT_X,XT_z)
-print(beta)
+
+z_predict = np.dot(X,beta)
+
+MSE = 1.0/float(N) * sum( (z_predict-z)**2 )
+print(MSE)

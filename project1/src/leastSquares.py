5a84eac8f275d4e145e5e43cca43d2bdf80caffb
Morten Ledum <mortele@student.matnat.uio.no>
2018-09-14 11:50:57 +0200

    Added predict and MSE methods to LeastSquares class, tested the former.

--- project1/src/leastSquares.py
+++ project1/src/leastSquares.py
@@ -1,28 +1,103 @@
 import numpy as np
-from sklearn import linear_model
+from sklearn import linear_model, metrics
 
 
 class LeastSquares :
     def __init__(self, backend='normal') :
-        self.backend = backend
+        self.backend        = backend
+        self.fitDone        = False
+
+        self.regression     = None
+        
+        self.X              = None
+        self.y              = None
+        self.yHat           = None
+        self.beta           = None
+        
+        self._MSE           = None
+        self._R2            = None
+        self._betaVariance  = None
 
     def setBackend(self, backend) :
         self.backend = backend
+        self.fitDone = False
 
-    def fit(self, X, y) :
+    def _checkManualBackend(self) :
         if self.backend == 'manual' :
-            self._manualFit(X,y)
+            return True
         elif ((self.backend == 'sklearn') or (self.backend == 'skl')):
-            self._sklFit(X,y)
+            return False
         else :
             raise ValueError('Backend <' + self.backend + '> not recognized.')
 
+    def _checkFitDoneAndManualBackend(self) :
+        manual = self._checkManualBackend()
+        if not self.fitDone :
+            raise Warning("This action requires a fit to be performed prior to being executed")
+        return manual
+
+    def fit(self, X, y) :
+        self.X = X
+        self.y = y
+
+        if self._checkManualBackend() :
+            self._manualFit(X,y)
+        else :
+            self._sklFit(X,y)
+        
+        self.fitDone = True
         return self.beta
 
-    def _sklFit(self, X,y) :
-        self.regression = linear_model.LinearRegression()
+    def _sklFit(self, X, y) :
+        self.regression = linear_model.LinearRegression(fit_intercept=True)
         self.regression.fit(X,y)
         self.beta = self.regression.coef_
+        self.beta[0] = self.regression.intercept_
 
     def _manualFit(self, X,y) :
         self.beta = np.dot(np.linalg.inv(np.dot(np.transpose(X),X)), np.dot(np.transpose(X),y))
+
+    def predict(self, X=None) :
+        if X != None :
+            self.X = X
+
+        if self._checkFitDoneAndManualBackend() :
+            self._manualPredict()
+        else :
+            self._sklPredict()
+        return self.yHat
+
+    def _manualPredict(self) :
+        self.yHat = np.dot(self.X, self.beta)
+
+    def _sklPredict(self) :
+        self.yHat = self.regression.predict(self.X)-self.beta[0]
+
+    def meanSquaredError(self) :
+        return self.MSE()
+
+    def MSE(self) :
+        if self._checkFitDoneAndManualBackend() :
+            self._manualMSE()
+        else :
+            self._sklMSE()
+        return self._MSE
+
+    def _manualMSE(self) :
+        if self.yHat == None :
+            self._manualPredict()
+        N = self.yHat.size
+        self._MSE = np.sum((self.y - self.yHat)**2) / N
+
+
+    def _sklMSE(self) :
+        if self.yHat == None :
+            self.yHat = self._sklPredict()
+        self._MSE = metrics.mean_squared_error(self.y, self.yHat)
+
+
+
+
+
+
+
--- project1/test/test_leastSquares.py
+++ project1/test/test_leastSquares.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pytest
 import numpy as np
+import random
+import matplotlib.pyplot as plt
 
 # Add the src/ directory to the python path so we can import the code 
 # we need to test directly as 'from <file name> import <function/class>'
@@ -11,37 +13,41 @@ from leastSquares import LeastSquares
 
 
 
-def test_franke() :
-    """Tests the Least Squares class as implemented in leastSquares.py
+def test_LeastSquares_fit() :
+    """Tests the fit method of the Least Squares class
 
     The tests comprise fitting of models to known data.
     """
 
-    # Ensure fitting polynomials of order 1 through 5 to y(x)=x results
+    # Ensure fitting polynomials of order 1 through 5 to y(x) = x results
     # in the beta corresponding to the x term equal to 1.0 and all other
     # beta values zero.
-    for method in ['manual', 'skl'] :
-        N = 10
-        x = np.linspace(0,1,N)
-        y = x
+    #
+    # Secondly we test on y(x)=x + 2 to also make sure the intercept is 
+    # calculated correctly.
+    for intercept in [0, 2] :
+        for method in ['manual', 'skl'] :
+            N = 10
+            x = np.linspace(0,1,N)
+            y = x + intercept
 
-        for i in range(2,5+1) :
-            P = i
-            X = np.zeros(shape=(N,P))
-            X[:,0] = 1.0
+            for i in range(2,5+1) :
+                P = i
+                X = np.zeros(shape=(N,P))
+                X[:,0] = 1.0
 
-            for j in range(1,P) :
-                X[:,j] = x**j
+                for j in range(1,P) :
+                    X[:,j] = x**j
 
-            print(method)
-            OLS = LeastSquares(backend=method)
-            beta = OLS.fit(X,y)
-            print(beta)
-            
-            assert beta[0] == pytest.approx(0.0, abs=1e-10)
-            assert beta[1] == pytest.approx(1.0, abs=1e-10)
-            for j in range(2,P) :
-                assert beta[j] == pytest.approx(0.0, abs=1e-10)
+                OLS = LeastSquares(backend=method)
+                beta = OLS.fit(X,y)
+                
+                assert beta[0] == pytest.approx(intercept, abs=1e-10)
+                assert beta[1] == pytest.approx(1.0, abs=1e-10)
+                for j in range(2,P) :
+                    assert beta[j] == pytest.approx(0.0, abs=1e-10)
+
+
 
     # Ensure the backend='manual' and the backend='skl' versions of 
     # LeastSquares.fit(X,y) give the same result.
@@ -55,17 +61,79 @@ def test_franke() :
     for j in range(1,P) :
         X[:,j] = x**j
 
-    print(X)
     OLS = LeastSquares(backend='manual')
     beta_manual = OLS.fit(X,y)
 
     OLS = LeastSquares(backend='skl')
     beta_skl    = OLS.fit(X,y)
 
-    print(beta_manual)
-    print(beta_skl)
-
     assert beta_manual == pytest.approx(beta_skl, abs=1e-10)
+
+def test_LeastSquares_predict() :
+    """Tests the predict method of the Least Squares class
+
+    The test is done with a known beta array, comparing results to a known
+    MSE value.
+    """
+    N = 5
+    P = 3
+    x = np.linspace(0,1,N)
+    random.seed(10)
+    y = 3*x**2 - 9*x - 2.4*x**5 - 3.1
+    X = np.zeros(shape=(N,P))
+    X[:,0] = 1.0
+
+    for j in range(1,P) :
+        X[:,j] = x**j
+
+    OLS = LeastSquares(backend='skl')
+    beta_skl    = OLS.fit(X,y)
+    predict_skl = OLS.predict()
+
+    OLS = LeastSquares(backend='manual')
+    beta_manual    = OLS.fit(X,y)
+
+    # Ensure the exact same beta value are used by both backend versions.
+    OLS.beta       = beta_skl
+    predict_manual = OLS.predict()
+
+    assert (predict_manual == pytest.approx(predict_skl, abs=1e-15))
+
+
+def test_LeastSquares_meanSquaredError() :
+    """Tests the meanSquaredError method of the Least Squares class
+
+    The test is done with a known beta array, comparing results to a known
+    MSE value.
+    
+    N = 5
+    P = 3
+    x = np.linspace(0,1,N)
+    random.seed(10)
+    y = 3*x**2 - 9*x - 2.4*x**5 - 3.1
+    X = np.zeros(shape=(N,P))
+    X[:,0] = 1.0
+
+    for j in range(1,P) :
+        X[:,j] = x**j
+
+    OLS = LeastSquares(backend='skl')
+    beta_skl    = OLS.fit(X,y)
+    MSE_skl     = OLS.meanSquaredError()
+
+    OLS = LeastSquares(backend='manual')
+    beta_manual = OLS.fit(X,y)
+
+    # Ensure the manual and the skl fit both use the exact same beta 
+    # values.
+    OLS.beta    = beta_skl
+    MSE_manual  = OLS.meanSquaredError()
+
+    assert MSE_skl == pytest.approx(MSE_manual, abs=1e-10)
+    
+    """
+
+
 
 
 

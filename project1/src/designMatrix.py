e5d9149b9f090d7b52dcb37523db11d79e642054
Morten Ledum <mortele@student.matnat.uio.no>
2018-09-20 10:58:06 +0200

    DesignMatrix class method getMatrixFunction implemented, in process of testing

--- project1/src/designMatrix.py
+++ project1/src/designMatrix.py
@@ -30,8 +30,8 @@ class DesignMatrix :
             Either a callable object or a string. In the latter case, only
             'polynomial' is accepted.
         degree : int
-            The degree of the polynomial model used, only considered in the
-            case of model being the string 'polynomial'.
+            The degree of the polynomial model used, or the number of functions
+            used in the case of a callable model input.
 
         Raises
         ------
@@ -53,8 +53,10 @@ class DesignMatrix :
 
         if callable(model) :
             self.type_ = 'function'
-            if degree != None :
-                raise Warning("When providing a callable object for the DesignMatrix, the degree parameter is ignored.")
+            if degree == None :
+                raise ValueError("The degree parameter must specify the number of different functions when initializing DesignMatrix with a callable object.")
+            if type(degree) != int :
+                raise ValueError("The specified number of different functions must be an integer.")
         else :
             if isinstance(model, str) :
                 if model == 'polynomial' :
@@ -73,14 +75,54 @@ class DesignMatrix :
 
 
     def getMatrix(self, x) :
+        """Compute the design matrix of the chosen type
+
+        Computes and returns the design matrix of either polynomial or
+        custom function type. If a callable object was given in the 
+        constructor, the custom function design matrix is setup. If the 
+        'polynomial' keyword was given, a polynomial design matrix of the
+        specified order is setup.
+
+        In the former case, the self._getMatrixFunction method is called,
+        while the latter results in a self._getMatrixPolynomial call.
+
+        Paramters
+        ---------
+        x : numpy.array
+            The data set, a 1D numpy array, used for the construction of
+            the design matrix
+
+        Returns
+        -------
+        self.matrix : numpy.array
+            The design matrix, a 2D numpy array
+        """
         if self.type_ == 'polynomial' :
             self._getMatrixPolynomial(x)
-        elif self._type_ == 'function' :
-            raise NotImplementedError("...")
+        elif self.type_ == 'function' :
+            self._getMatrixFunction(x)
         return self.matrix
 
 
     def _getMatrixPolynomial(self, x) :
+        """Computes the design matrix for a polynomial of a given degree
+
+        Computes the design matrix of polynomial type with degree specified 
+        in the input to the constructor. The first column contains unity, 
+        the subsequent columns are evaluated as 
+
+            ╭     ╮      j
+            │  X  │ =  x  
+            ╰   ij╯     i
+        
+        with X being the design matrix and x being the input data set.
+        
+        Paramters
+        ---------
+        x : numpy.array
+            The data set, a 1D numpy array, used for the construction of
+            the design matrix
+        """
         N = x.size
         P = self.degree
         self.matrix = np.zeros(shape=(N,P+1))
@@ -89,6 +131,34 @@ class DesignMatrix :
             self.matrix[:,j] = x**j
 
 
+    def _getMatrixFunction(self, x) :
+        """Computes the design matrix for a given input function
+
+        Computes the design matrix type function, evaluation the function
+        input in the constructor at the data points given in the input. The
+        first column contains unity, the subsequent columns are evaluated
+        as 
+
+            ╭     ╮      ╭   ╮
+            │  X  │ =  f │ x │
+            ╰   ij╯     j╰  i╯
+        
+        with X being the design matrix and x being the input data set. The 
+        function f subscripted j is the j-th function. 
+        
+        Paramters
+        ---------
+        x : numpy.array
+            The data set, a 1D numpy array, used for the construction of
+            the design matrix
+        """
+        N = x.size
+        P = self.degree
+        self.matrix = np.zeros(shape=(N,P+1))
+        self.matrix[:,0] = 1.0
+        for i in range(N) :
+            for j in range(1,P+1) :
+                self.matrix[i,j] = self.model(j, x[i])
 
 
 
--- project1/test/test_designMatrix.py
+++ project1/test/test_designMatrix.py
@@ -76,3 +76,64 @@ def test_DesignMatrix_polynomial() :
                         [1.0,    7.0,   49.0,  343.0, 2401.0,  16807.0,  117649.0] ])
     assert X == pytest.approx(X_true, abs=1e-15)
 
+
+def test_DesignMatrix_function() :
+    """Tests the function method of the DesignMatrix class
+
+    The tests comprise setting up design matrices with different
+    functions and comparing to manually setup matrices.
+    """
+    class f1 :
+        def __init__(self, degree) :
+            self.degree = degree
+
+        def __call__(self, i, x) :
+            if i > self.degree :
+                raise ValueError("Specified function index is larger than the number of available functions.")
+            if i == 0 :
+                return self._f0(x)
+            elif i == 1 :
+                return self._f1(x)
+            elif i == 2 :
+                return self._f2(x)
+            elif i == 3 :
+                return self._f3(x)
+            elif i == 4 : 
+                return self._f4(x)
+            elif i == 5 : 
+                return self._f5(x) 
+            elif i == 6 :
+                return self._f6(x)
+
+        def _f1(self, x) :
+            return np.cos(x)
+        def _f2(self, x) :
+            return np.sin(x)
+        def _f3(self, x) :
+            return np.tan(x)
+        def _f4(self, x) :
+            return np.cosh(x)
+        def _f5(self, x) :
+            return np.sinh(x)
+        def _f6(self, x) :
+            return np.tanh(x)
+
+    numberOfFunctions = 1
+    f = f1(numberOfFunctions)
+    x = np.array([2.0])
+    DM = DesignMatrix(f, numberOfFunctions)
+    X  = DM.getMatrix(x)
+    X_true = np.array([ [1.0,  np.cos(2.0)] ])
+    assert X == pytest.approx(X_true, abs=1e-15)
+
+
+
+
+
+
+
+
+
+
+
+

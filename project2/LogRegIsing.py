58cc648b510675bd7ca6dd7fb02ca9210133676d
haakoek <haakoek@student.matnat.uio.no>
2018-10-25 11:02:53 +0200

    Added logistic function

--- project2/LogRegIsing.py
+++ project2/LogRegIsing.py
@@ -3,7 +3,32 @@ import pickle
 import sklearn.linear_model as skl
 import sklearn.metrics as skm
 import sklearn.model_selection as skms
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+def vizualise():
+	# set colourbar map
+	cmap_args=dict(cmap='plasma_r')
+
+	# plot states
+	fig, axarr = plt.subplots(nrows=1, ncols=3)
+
+	plt.figure(1)
+	plt.subplot(311)
+	plt.imshow(X_train_ordered[20001].reshape(L,L),**cmap_args)
+
+	plt.subplot(312)
+	plt.imshow(X_critical[10001].reshape(L,L),**cmap_args)
+
+	plt.subplot(313)
+	plt.imshow(X_train_disordered[50001].reshape(L,L),**cmap_args)
+
+	plt.show()
+
+def logistic(s):
+	return np.exp(s)/(1+np.exp(s))
+
+np.random.seed(1)
 L = 40 #Nr of spins 40x40
 
 label_filename = "data/Ising2DFM_reSample_L40_T=All_labels.pkl"
@@ -25,30 +50,8 @@ data[data == 0] = -1
 print(data.shape)
 print(labels.shape)
 
-# Set up slices of the dataset
-ordered_train = slice(0, 65000)
-ordered_test  = slice(65000,70000)
-
-critical = slice(70000, 100000)
-
-disordered_train = slice(100000, 152000)
-disordered_test = slice(152000,160000)
-
-#Split data set in training and testing set
-X_train_ordered    = data[ordered_train]
-X_train_disordered = data[disordered_train]
-y_train_ordered = labels[ordered_train]
-y_train_disordered = labels[disordered_train]
-
-X_critical = data[critical]
-y_critical = data[critical]
-
-X_test_ordered    = data[ordered_test]
-X_test_disordered = data[disordered_test]
-y_test_ordered = labels[ordered_test]
-y_test_disordered = labels[disordered_test]
-
 """
+#Uses more memory but more compact
 X_train, X_test, y_train, y_test = skms.train_test_split(
 	np.concatenate((data[ordered], data[disordered])),
 	np.concatenate((labels[ordered], labels[disordered])),
@@ -61,29 +64,33 @@ print(y_train.shape)
 print(y_test.shape)
 """
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+# Set up slices of the dataset
+ordered_train = slice(0, 65000)
+ordered_test  = slice(65000,70000)
+critical = slice(70000, 100000)
+disordered_train = slice(100000, 152000)
+disordered_test = slice(152000,160000)
 
-# set colourbar map
-cmap_args=dict(cmap='plasma_r')
+#Split data set in training and testing set
+X_train_ordered    = data[ordered_train]
+X_train_disordered = data[disordered_train]
+y_train_ordered    = labels[ordered_train]
+y_train_disordered = labels[disordered_train]
 
-# plot states
-fig, axarr = plt.subplots(nrows=1, ncols=3)
+#The critical phases are left out the training
+X_critical = data[critical]
+y_critical = data[critical]
 
-plt.figure(1)
-plt.subplot(311)
-plt.imshow(X_train_ordered[20001].reshape(L,L),**cmap_args)
+X_test_ordered    = data[ordered_test]
+X_test_disordered = data[disordered_test]
+y_test_ordered    = labels[ordered_test]
+y_test_disordered = labels[disordered_test]
 
+#vizualise()
 
-plt.subplot(312)
-plt.imshow(X_critical[10001].reshape(L,L),**cmap_args)
-
-plt.subplot(313)
-plt.imshow(X_train_disordered[50001].reshape(L,L),**cmap_args)
-
-
-
-plt.show()
-
+#Initialize beta-parameters
+beta = np.random.randn(L*L)
+val  = np.dot(X_train_disordered[100],beta)
+print(logistic(val))
 
 

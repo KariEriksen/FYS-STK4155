78f11901b893b771106d5553f563bca12d95b55b
haakoek <haakoek@student.matnat.uio.no>
2018-10-28 01:15:37 +0200

    Added some more output from Logreg

--- project2/LogRegIsing.py
+++ project2/LogRegIsing.py
@@ -10,47 +10,47 @@ import sys
 import time
 
 def vizualise():
-	# set colourbar map
-	cmap_args=dict(cmap='plasma_r')
+    # set colourbar map
+    cmap_args=dict(cmap='plasma_r')
 
-	# plot states
-	fig, axarr = plt.subplots(nrows=1, ncols=3)
+    # plot states
+    fig, axarr = plt.subplots(nrows=1, ncols=3)
 
-	plt.figure(1)
-	plt.subplot(311)
-	plt.imshow(X_train_ordered[20001].reshape(L,L),**cmap_args)
+    plt.figure(1)
+    plt.subplot(311)
+    plt.imshow(X_train_ordered[20001].reshape(L,L),**cmap_args)
 
-	plt.subplot(312)
-	plt.imshow(X_critical[10001].reshape(L,L),**cmap_args)
+    plt.subplot(312)
+    plt.imshow(X_critical[10001].reshape(L,L),**cmap_args)
 
-	plt.subplot(313)
-	plt.imshow(X_train_disordered[50001].reshape(L,L),**cmap_args)
+    plt.subplot(313)
+    plt.imshow(X_train_disordered[50001].reshape(L,L),**cmap_args)
 
-	plt.show()
+    plt.show()
 
 def logistic(s):
-	return np.exp(s)/(1.0+np.exp(s))
+    return np.exp(s)/(1.0+np.exp(s))
 
 def plot_logisticFunction():
-	
-	s = np.linspace(-10,10,100)
-	plt.plot(s,logistic(s))
-	plt.show()
+    
+    s = np.linspace(-10,10,100)
+    plt.plot(s,logistic(s))
+    plt.show()
 
 
 def crossEntropy(beta,X,y):
-	
-	Cbeta = 0
-	
-	for i in range(len(y)):
-		tmp    = np.dot(X[i],beta)
-		val    = y[i]*tmp - np.log(1+np.exp(tmp))
-		Cbeta -= val
-	
-	return Cbeta
+    
+    Cbeta = 0
+    
+    for i in range(len(y)):
+        tmp    = np.dot(X[i],beta)
+        val    = y[i]*tmp - np.log(1+np.exp(tmp))
+        Cbeta -= val
+    
+    return Cbeta
 
 def gradientCrossEntropy(X,p,y):
-	return -np.dot(X.T,y-p)/float(X.shape[0])
+    return -np.dot(X.T,y-p)/float(X.shape[0])
 
 
 
@@ -62,31 +62,34 @@ dat_filename   = "data/Ising2DFM_reSample_L40_T=All.pkl"
 
 # Read in the labels
 with open(label_filename, "rb") as f:
-	labels = pickle.load(f)
+    labels = pickle.load(f)
 
 # Read in the corresponding configurations
 with open(dat_filename, "rb") as f:
-	data = np.unpackbits(pickle.load(f)).reshape(-1, 1600).astype("int")
+    data = np.unpackbits(pickle.load(f)).reshape(-1, 1600).astype("int")
 
 data[data == 0] = -1
 
 # Set up slices of the dataset
-ordered    = slice(0, 10000)
+ordered    = slice(0, 70000)
 critical   = slice(70000, 100000)
-disordered = slice(100000,110000)
+disordered = slice(100000,160000)
 
 #Uses more memory but more compact
 X_train, X_test, y_train, y_test = skms.train_test_split(
-	np.concatenate((data[ordered], data[disordered])),
-	np.concatenate((labels[ordered], labels[disordered])),
-	test_size=0.05
+    np.concatenate((data[ordered], data[disordered])),
+    np.concatenate((labels[ordered], labels[disordered])),
+    test_size=0.5
 )
+
+X_critical = data[70000:100000,:]
+y_critical = labels[70000:100000]
 
 del data, labels
 
 X_train = np.c_[np.ones(X_train.shape[0]), X_train]
 X_test = np.c_[np.ones(X_test.shape[0]), X_test]
-
+X_critical = np.c_[np.ones(X_critical.shape[0]), X_critical]
 print(X_train.shape)
 print(X_test.shape)
 print(y_train.shape)
@@ -96,21 +99,31 @@ print(y_test.shape)
 
 #Initialize beta-parameters
 beta   = np.random.uniform(-0.5,0.5,L*L+1)
+beta   = beta/np.linalg.norm(beta)
 eta    = 0.005
 norm   = 100
-Lambda = 0
+Lambda = 1e5
 
-for i in range(0,100):
+for i in range(0,800):
 
-	p_hat  = logistic(np.dot(X_train,beta))
-	gradC  = gradientCrossEntropy(X_train,p_hat,y_train)
-	gradC += 2*Lambda*beta
-	beta   = beta - eta*gradC
-	norm   = np.linalg.norm(gradC)
-	
-	print(norm, i)
+    p_hat  = logistic(np.dot(X_train,beta))
+    gradC  = gradientCrossEntropy(X_train,p_hat,y_train)
+    gradC += 2*Lambda*beta/X_train.shape[0]
+    beta   = beta - eta*gradC
+    norm   = np.linalg.norm(gradC)
+    print(norm, np.linalg.norm(beta), i)
+    if(norm < 1e-5):
+        break
 
 
 p_predict = logistic(np.dot(X_train,beta))
+p_test    = logistic(np.dot(X_test,beta))
+p_critical= logistic(np.dot(X_critical,beta))
+
 train_accuracy = np.sum( (p_predict > 0.5)  == y_train )/float(X_train.shape[0])
-print("Training accuracy: %g" % train_accuracy)
+test_accuracy  = np.sum( (p_test > 0.5)  == y_test )/float(X_test.shape[0])
+crit_accuracy  = np.sum( (p_critical > 0.5)  == y_critical )/float(X_critical.shape[0])
+
+print("Training accuracy: %.6f" % train_accuracy)
+print("Test accuracy: %.6f" % test_accuracy)
+print("Critical accuracy: %g" % crit_accuracy)

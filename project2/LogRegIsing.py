843c2d2b3be40d8076ce1514a2dbea864f9588b2
haakoek <haakoek@student.matnat.uio.no>
2018-11-07 18:21:44 +0100

    Optimal param for GD in LogReg, final plot added, nice agreement with Mehta

--- project2/LogRegIsing.py
+++ project2/LogRegIsing.py
@@ -9,31 +9,62 @@ import numba
 import sys
 import time
 from scipy.special import expit
+from scipy.optimize import minimize
+
+def cost_function(beta,X,y):
+    p    = np.dot(X, beta)
+    loss = -np.sum(y*p - np.log(1+np.exp(p)))   #+ 0.5*np.dot(beta,beta)*Lambda
+    return loss
 
 
-def gradientAscent(X,y,Lambda,eta=5e-7,max_iters=150,tolerance=1e-4):
+def gradientDescent(X,y,Lambda,eta=1e-4,max_iters=150,tolerance=1e-4,scale=1.0):
     
     #Initialize beta-parameters
-    beta   = np.zeros(X.shape[1])
-    norm   = 100
+    beta    = np.random.uniform(-0.5,0.5,X.shape[1])
+    beta[0] = 1
+    beta /= np.linalg.norm(beta)
+    beta_prev = beta.copy()
+
+    norm    = 100
+    eta_k   = 0
     
-    for i in range(0,max_iters):
+    #first step
+    z = np.dot(X, beta)
+    p = expit(z)
+    gradient = -np.dot(X.T,y-p)/scale + Lambda*beta/scale
+    beta    -= eta*gradient
+    norm     = np.linalg.norm(gradient)
+    
+    gradient_prev = gradient.copy()
+    print(norm,scale*norm)
+    
+    for k in range(1,max_iters):
         
         z = np.dot(X, beta)
         p = expit(z)
 
-        gradient = np.dot(X.T,y-p) - Lambda*beta
-        beta    += eta*gradient
-        norm     = np.linalg.norm(gradient)
+        gradient_prev = gradient.copy()
+        gradient      = -np.dot(X.T,y-p)/scale + Lambda*beta/scale
         
-        if(i%1000 == 0):
-            print(norm)
+        eta_k         = np.dot((beta - beta_prev),gradient-gradient_prev) / np.linalg.norm(gradient-gradient_prev)**2
 
-        if(norm < tolerance):
-            print("Gradient ascent converged to given precision in %d iterations" % i)
+        beta_prev     = beta.copy()
+        beta         -= eta_k*gradient
+        
+        norm          = np.linalg.norm(gradient)
+        
+        if(k%10 == 0):
+            print(norm,scale*norm)
+
+        if(scale*norm < tolerance):
+            print("Gradient Descent converged to given precision in %d iterations" % k)
             break
+
+        
+        
     
     return beta, norm
+
 
 np.random.seed(12)
 L = 40 #Nr of spins 40x40
@@ -52,9 +83,9 @@ with open(dat_filename, "rb") as f:
 data[data     == 0] = -1
 
 # Set up slices of the dataset
-ordered    = slice(0     , 5000 )
+ordered    = slice(0     , 70000 )
 critical   = slice(70000 , 100000)
-disordered = slice(100000, 104900)
+disordered = slice(100000, 160000)
 
 
 X_train, X_test, y_train, y_test = skms.train_test_split(
@@ -68,58 +99,88 @@ y_critical = labels[critical]
 
 del data, labels
 
+#Add intercept column
+X_train    = np.c_[np.ones(X_train.shape[0])   , X_train]
+X_test     = np.c_[np.ones(X_test.shape[0])    , X_test]
+X_critical = np.c_[np.ones(X_critical.shape[0]), X_critical]
 
-Lambda = 1
 
-logreg = skl.LogisticRegression(fit_intercept=True,C=1.0/Lambda,tol=1e-8)
+# define regularisation parameter
+
+lmbdas = np.array([1e5]) #np.logspace(-5,4,10)
+
+# preallocate data
+train_accuracy=np.zeros(lmbdas.shape,np.float64)
+test_accuracy=np.zeros(lmbdas.shape,np.float64)
+critical_accuracy=np.zeros(lmbdas.shape,np.float64)
+
+"""
+logreg = skl.LogisticRegression(C=C,random_state=1,verbose=0,max_iter=1E3,tol=1E-8)
 logreg.fit(X_train,y_train)
 beta_scikit = np.zeros(X_train.shape[1]+1)
 beta_scikit[0] = logreg.intercept_
 beta_scikit[1:] = logreg.coef_[0]
-print(beta_scikit[0:10])
 
-# check accuracy
 train_accuracy    = logreg.score(X_train,y_train)
 test_accuracy     = logreg.score(X_test,y_test)
-crit_accuracy = logreg.score(X_critical,y_critical)
-print("Training accuracy (skl): %.6f" % train_accuracy)
-print("Test accuracy     (skl): %.6f" % test_accuracy)
-print("Critical accuracy (skl): %g" % crit_accuracy)
-
-sys.exit(1)
-#Add intercept column
-X_train    = np.c_[np.ones(X_train.shape[0]), X_train]
-X_test     = np.c_[np.ones(X_test.shape[0]), X_test]
-X_critical = np.c_[np.ones(X_critical.shape[0]), X_critical]
-print(X_train.shape)
-
-beta,norm  = gradientAscent(X_train,y_train,Lambda=Lambda,max_iters=100000,tolerance=1e-4)
-
-p_predict  = expit(np.dot(X_train,beta))
-p_test     = expit(np.dot(X_test,beta))
-p_critical = expit(np.dot(X_critical,beta))
-
-train_accuracy = ( (p_predict  > 0.5) == y_train     ).mean()
-test_accuracy  = ( (p_test     > 0.5) == y_test      ).mean()
-crit_accuracy  = ( (p_critical > 0.5) == y_critical  ).mean()
-
-#print("Lambda: %g" % lmbda)
-print("norm(gradC): %g" % norm)
-print("Training accuracy: %.6f" % train_accuracy)
-print("Test accuracy: %.6f" % test_accuracy)
-print("Critical accuracy: %g" % crit_accuracy)
-
-
-
-print(beta[0])
-print(beta[1:4])
-
-
+crit_accuracy     = logreg.score(X_critical,y_critical)
 """
+
+# loop over regularisation strength
+for i,Lambda in enumerate(lmbdas):
+    
+    beta,norm  = gradientDescent(X_train,y_train,Lambda=Lambda,eta=1e-5,max_iters=100000,tolerance=1e-4,scale=X_train.shape[0])
+
+    p_predict  = expit(np.dot(X_train,beta))
+    p_test     = expit(np.dot(X_test,beta))
+    p_critical = expit(np.dot(X_critical,beta))
+
+
+    # check accuracy
+    print("Lambda: %g" % Lambda)
+
+    
+    print("Training accuracy (skl): %.6f" % train_accuracy)
+    print("Test accuracy     (skl): %.6f" % test_accuracy)
+    print("Critical accuracy (skl): %g" % critical_accuracy)
+    
+
+    train_accuracy[i] = ( (p_predict  > 0.5) == y_train     ).mean()
+    test_accuracy[i]  = ( (p_test     > 0.5) == y_test      ).mean()
+    critical_accuracy[i]  = ( (p_critical > 0.5) == y_critical  ).mean()
+
+    print("Training accuracy  (GD): %.6f" % train_accuracy[i])
+    print("Test accuracy      (GD): %.6f" % test_accuracy[i])
+    print("Critical accuracy  (GD): %g" % critical_accuracy[i])
+
+
+#np.save("train_acc",train_accuracy)
+#np.save("test_acc",test_accuracy)
+#np.save("crit_acc",critical_accuracy)
+#print(beta_scikit[0:10])
+#print(beta[0:10])
+
+train_accuracy_old = np.load("train_acc_old.npy")
+test_accuracy_old = np.load("test_acc_old.npy")
+critical_accuracy_old = np.load("crit_acc_old.npy")
+
+train_accuracy_new = np.zeros(11)
+test_accuracy_new  = np.zeros(11)
+critical_accuracy_new = np.zeros(11) 
+
+train_accuracy_new[0:10] = train_accuracy_old
+test_accuracy_new[0:10] = test_accuracy_old
+critical_accuracy_new[0:10] = critical_accuracy_old
+
+train_accuracy_new[10] = train_accuracy[0]
+test_accuracy_new[10] = test_accuracy[0]
+critical_accuracy_new[10] = critical_accuracy[0]
+
+lmbdas = np.logspace(-5,5,11)
 # plot accuracy against regularisation strength
-plt.plot(lmbdas,train_accuracy,'*-b',label='Train')
-plt.plot(lmbdas,test_accuracy,'*-r',label='Test')
-plt.plot(lmbdas,crit_accuracy,'*-g',label='Critical')
+plt.semilogx(lmbdas,train_accuracy_new,'*-b',label='Train')
+plt.semilogx(lmbdas,test_accuracy_new,'*-r',label='Test')
+plt.semilogx(lmbdas,critical_accuracy_new,'*-g',label='Critical')
 
 plt.xlabel('$\\lambda$')
 plt.ylabel('$\\mathrm{accuracy}$')
@@ -128,4 +189,3 @@ plt.grid()
 plt.legend()
 plt.savefig("figures/logReg_acc_vs_regstrength.png")
 plt.show()
-"""
